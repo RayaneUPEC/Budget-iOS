901bd8658ee09f5681a5103f965aec22cf44e3ec
RayaneUPEC <rayane.khatim@etu.u-pec.fr>
2025-04-03 03:34:02 +0200

    Initial Commit

---
+++ BudgetTests/BudgetTests.swift
@@ -0,0 +1,16 @@
+//
+//  BudgetTests.swift
+//  BudgetTests
+//
+//  Created by Rayane KHATIM on 03/04/2025.
+//
+
+import Testing
+
+struct BudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
